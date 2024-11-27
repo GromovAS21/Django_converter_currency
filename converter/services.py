e377abe46010008294b7e6d98b07748a909654bf
Anton Gromov <seed14112@yandex.ru>
2024-11-27 16:52:46 +0300

    Добавление функции парсинга c сайта https://fixer.io

---
+++ converter/services.py
@@ -0,0 +1,14 @@
+import requests
+
+from config.settings import API_ACCESS_TOKEN
+
+TOKEN = API_ACCESS_TOKEN
+
+def get_current_currency(cur_from, cur_to):
+    """
+    Запрос о текущем курсе с сайта https://fixer.io
+    """
+
+    url = f"http://data.fixer.io/api/latest?access_key={TOKEN}&base={cur_from}&symbols={cur_to}"
+    response = requests.get(url)
+    return response.json()["rates"][cur_to]
